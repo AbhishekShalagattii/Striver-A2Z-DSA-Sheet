623c9809c6ef65c3f6556507d34d27b44b168867
Abhishek Halagatti <abhisheksh327507@gmail.com>
2026-08-27 17:15:56 +0530

    For array easy Q10-Added the brute force appraoch

--- Step3_ProblemsOnArrays/EasyProblems/Q10_FindMissingNumber.cpp
+++ Step3_ProblemsOnArrays/EasyProblems/Q10_FindMissingNumber.cpp
@@ -4,17 +4,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Brute approach
 int findMissNum(int arr[],int n)
 {
-    int xor1 = 0, xor2 = 0;
-    for(int i = 0; i < n - 1; i++)
+    for(int i = 1; i < n; i++)
     {
-        xor2 = xor2 ^ arr[i];
-        xor1 = xor1 ^ (i+1);
+        int flg = 0;
+        for(int j = 0; j < n-1; j++ )
+        {
+            if(arr[j] == i)
+            {
+                flg = 1;
+                break;
+            }
+        }
+        if(flg == 0)
+        {
+            return i;
+        }
     }
-    xor1 = xor1 ^ n;
-    return xor1 ^ xor2;
 }
+
+//Optimal approach
+// int findMissNum(int arr[],int n)
+// {
+//     int xor1 = 0, xor2 = 0;
+//     for(int i = 0; i < n - 1; i++)
+//     {
+//         xor2 = xor2 ^ arr[i];
+//         xor1 = xor1 ^ (i+1);
+//     }
+//     xor1 = xor1 ^ n;
+//     return xor1 ^ xor2;
+// }
 
 int main()
 {
