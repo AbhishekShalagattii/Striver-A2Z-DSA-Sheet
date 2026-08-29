69cf66ba9b09f53a0b1df09ff2abc92ed23d011a
Abhishek Halagatti <abhisheksh327507@gmail.com>
2026-08-29 17:01:04 +0530

    for HardArr Q2 - Added the optimal approach

--- Step3_ProblemsOnArrays/Hard/Q2_majorityElems2.cpp
+++ Step3_ProblemsOnArrays/Hard/Q2_majorityElems2.cpp
@@ -2,25 +2,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Betterr approach
+// vector<int> getMajority(vector<int> &nums)
+// {
+//     int n = nums.size();
+//     int minFre = int (n/3) + 1;
+//     vector<int> ans;
+//     unordered_map<int,int> mpp;
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         mpp[nums[i]]++;
+//         if(mpp[nums[i]] == minFre)
+//         {
+//             ans.push_back(nums[i]);
+//         }
+//     }
+//     return ans;
+
+// }
+
 vector<int> getMajority(vector<int> &nums)
 {
     int n = nums.size();
-    int minFre = int (n/3) + 1;
+    int cnt1 = 0,cnt2 = 0;
+    int elem1,elem2 = INT_MIN;
+    int mini = (int)(n/3)+1;
+    
     vector<int> ans;
-    unordered_map<int,int> mpp;
 
     for(int i = 0; i < n; i++)
     {
-        mpp[nums[i]]++;
-        if(mpp[nums[i]] == minFre)
+        if(cnt1 == 0 && nums[i] != elem2)
         {
-            ans.push_back(nums[i]);
+            elem1 = nums[i];
+            cnt1 = 1;
+        }
+        else if(cnt2 == 0 && nums[i] != elem1)
+        {
+            elem2 = nums[i];
+            cnt2 = 1;
+        }
+        else if(nums[i] == elem1)
+        {
+            cnt1++;
+        }
+        else if(nums[i] == elem2)
+        {
+            cnt2++;
+        }
+        else{
+            cnt1--,cnt2--;
         }
     }
+
+    ans.push_back(elem1);
+    ans.push_back(elem2);
     return ans;
-
 }
-
 
 int main()
 {
