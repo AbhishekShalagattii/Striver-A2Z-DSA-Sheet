c9858b1191d087f5c0a84f510d07029a88a2dbc5
Abhishek Halagatti <abhisheksh327507@gmail.com>
2026-09-12 17:54:54 +0530

    Arr-Hard Q9_findMissing and repeating numbers optimal sol1 using basic maths solved and added

--- Step3_ProblemsOnArrays/Hard/Q9_findMissingAndRepeatingNum.cpp
+++ Step3_ProblemsOnArrays/Hard/Q9_findMissingAndRepeatingNum.cpp
@@ -2,28 +2,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Better Approach using the hashArr
+//Better Approach using the hashArr Tc~O(2N) SC~O(N)
+// vector<int> findMissAndRepeatNums(vector<int> &nums)
+// {
+//     int n = nums.size();
+//     int hashArr[n+1] = {0};
+
+//     for( int i = 0; i < n; i++)
+//     {
+//         hashArr[nums[i]]++;
+//     }
+
+//     int missing = -1, repeating = -1;
+
+//     for( int i = 1; i <= n; i++)
+//     {
+//         if(hashArr[i] == 0) missing = i;
+//         else if(hashArr[i] == 2) repeating = i;
+
+//         if(repeating != -1 && missing != -1) break;
+//     }
+
+//     return {repeating,missing};
+// }
+
 vector<int> findMissAndRepeatNums(vector<int> &nums)
 {
     int n = nums.size();
-    int hashArr[n+1] = {0};
+    int SN = (n*(n+1))/2;
+    int S2N = (n*(n+1)*(2*n+1))/6;
+    int S = 0, S2 = 0;
 
     for( int i = 0; i < n; i++)
     {
-        hashArr[nums[i]]++;
+        S += nums[i];
+        S2 += nums[i] * nums[i];
     }
 
-    int missing = -1, repeating = -1;
-
-    for( int i = 1; i <= n; i++)
-    {
-        if(hashArr[i] == 0) missing = i;
-        else if(hashArr[i] == 2) repeating = i;
-
-        if(repeating != -1 && missing != -1) break;
-    }
-
-    return {repeating,missing};
+    int val1 = S - SN;
+    int val2 = S2 - S2N;
+    val2 = val2/val1;
+    int x = (val1+val2)/2;
+    int y = x - val1;
+    return {x,y};
 }
 
 int main()
